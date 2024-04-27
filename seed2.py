300142c2328212a6cc275cb345c45ba37bde2d82
ejgbadebo0 <ejgbadebo@gmail.com>
2024-04-27 19:18:29 -0400

    debug seed2

--- seed2.py
+++ seed2.py
@@ -11,12 +11,15 @@ for m in range(1, m['count']):
     try:
         r = requests.get(f"{move_url}/{m}/")
         mov = r.json()
-
+        print(f'GET SUCCEEDED: {mov['id']}')
         m_name = format_move_name(mov['name'])
+        print(m_name)
         m_power = int(mov['power'])
+        print(m_power)
         m_pp = int(mov['pp'])
+        print(m_pp)
         m_accuracy = int(mov['accuracy'])
-
+        print(m_accuracy)
         new_move = Move(name=m_name,
                         power=m_power,
                         pp=m_pp,
@@ -28,9 +31,9 @@ for m in range(1, m['count']):
         for i in mov['learned_by_pokemon']:
             try:
                 pkm_id = i['url'].replace(base_url,'').replace('/','')
-                pokemon = Pokemon.query.get(pkm_id)
-                print(pkm_id)
-
+                print(f"PID: {pkm_id}")
+                pokemon = Pokemon.query.get(int(pkm_id))
+                
                 if pokemon: 
                     new_pokemon_move = PokemonMove(pokemon_id=int(pkm_id), move_id=mov['id'])
                     db.session.add(new_pokemon_move)
